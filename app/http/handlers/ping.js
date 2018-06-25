7822b56dead6d4f341445b0add1fa47996c20a44
Jared Hanson <jaredhanson@gmail.com>
2018-06-25 12:55:02 -0400

    Initial commit.

---
+++ app/http/handlers/ping.js
@@ -0,0 +1,50 @@
+/**
+ * TrackBack ping handler.
+ *
+ * This component provides an HTTP handler that receives TrackBack pings.
+ *
+ * In the hexagonal architecture promoted by Bixby.js, this handler drives a
+ * linkback port.  The actual adapter that will be utilized is injected by the
+ * IoC container, and is expected to be implemented by the application.
+ */
+exports = module.exports = function(linkbacks, parse, authenticate) {
+  var xml = require('xmlbuilder');
+  
+  
+  // TODO: Error if no URL specificifed
+  
+  function resolveTarget(req, res, next) {
+    req.locals.target = 'http://www.example.com/' + req.params[0];
+    next();
+  }
+  
+  function handle(req, res, next) {
+    // TODO: Add title, exerpt, blog name, and language options
+    
+    linkbacks.link(req.body.url, req.locals.target, function(err) {
+      // TODO: handle errors
+      //res.status(202).send('http://alice.host/webmentions/222')
+      
+      var doc = xml.create('response', { encoding: 'utf-8' })
+        .ele('error', '0')
+        .end({ pretty: true});
+      
+      res.status(200).send(doc)
+    });
+  }
+  
+  // curl --data "source=http://bob.host/post-by-bob&target=http://alice.host/post-by-alice" http://127.0.0.1:8080/
+  
+  return [
+    parse('application/x-www-form-urlencoded'),
+    authenticate([ 'anonymous' ]),
+    resolveTarget,
+    handle
+  ];
+}
+
+exports['@require'] = [
+  'http://schemas.modulate.io/js/social/ILinkbackService'
+  'http://i.bixbyjs.org/http/middleware/parse',
+  'http://i.bixbyjs.org/http/middleware/authenticate',
+];
---
+++ app/http/service.js
@@ -0,0 +1,32 @@
+/**
+ * TrackBack service.
+ *
+ * This component provides an HTTP service that implements [TrackBack](http://archive.cweiske.de/trackback/trackback-1.2.html).
+ * TrackBack is a way to receive a notification when one web resource links to
+ * another resource.
+ *
+ * In the hexagonal architecture promoted by Bixby.js, this service is an
+ * adapter to a linkback port.  The adapter implements TrackBack, which is
+ * suitable for use by user agents and remote services to notify the application
+ * of links back to application resources.
+ *
+ * References:
+ *   - [Trackback](https://en.wikipedia.org/wiki/Trackback)
+ */
+exports = module.exports = function(pingHandler) {
+  var express = require('express');
+  var router = new express.Router();
+  
+  router.post('/*', pingHandler);
+  
+  return router;
+};
+
+exports['@implements'] = [
+  'http://i.bixbyjs.org/http/Service',
+  'http://schemas.modulate.io/js/http/TrackBackService'
+];
+exports['@path'] = '/trackback';
+exports['@require'] = [
+  './handlers/ping'
+];
