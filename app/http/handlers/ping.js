0277cc78d32d2288e988bdebe26df779e5718885
Jared Hanson <jaredhanson@gmail.com>
2018-07-03 08:46:15 -0700

    Respond with error.

--- app/http/handlers/ping.js
+++ app/http/handlers/ping.js
@@ -23,8 +23,12 @@ exports = module.exports = function(linkbacks, parse, errorLogging, authenticate
     // TODO: Add title, exerpt, blog name, and language options
     
     linkbacks.ping(req.body.url, req.locals.target, function(err) {
+      if (err) { return next(err); }
+      
       // TODO: handle errors
       //res.status(202).send('http://alice.host/webmentions/222')
+      
+      
       
       var doc = xml.create('response', { encoding: 'utf-8' })
         .ele('error', '0')
