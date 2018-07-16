0ffe00e7f7feec3dc27e1f2daf4e696a7e066d7e
Jared Hanson <jaredhanson@gmail.com>
2018-07-16 13:42:12 -0700

    Clean up.

--- app/http/handlers/ping.js
+++ app/http/handlers/ping.js
@@ -25,11 +25,6 @@ exports = module.exports = function(linkbacks, parse, /*authenticate,*/ errorLog
     linkbacks.ping(req.body.url, req.locals.target, function(err) {
       if (err) { return next(err); }
       
-      // TODO: handle errors
-      //res.status(202).send('http://alice.host/webmentions/222')
-      
-      
-      
       var doc = xml.create('response', { encoding: 'utf-8' })
         .ele('error', '0')
         .end({ pretty: true });
@@ -49,7 +44,8 @@ exports = module.exports = function(linkbacks, parse, /*authenticate,*/ errorLog
     res.status(500).send(doc)
   }
   
-  // curl --data "source=http://bob.host/post-by-bob&target=http://alice.host/post-by-alice" http://127.0.0.1:8080/
+  
+  
   
   // curl --data "title=Foo+Bar&url=http://www.bar.com/&excerpt=My+Excerpt&blog_name=Foo" http://127.0.0.1:8080/trackback
   
